9a838fbe237313a4fa70adf9814a09e4db3c478a
LegendaHluPa <59069452+LegendaHluPa@users.noreply.github.com>
2022-10-26 22:55:45 +0300

    Add files via upload

---
+++ RelifeTrade/PermTrading/src/main/java/net/legendahlupa/com/permtrading/Config/CreatePermissionFile.java
@@ -0,0 +1,14 @@
+package net.legendahlupa.com.permtrading.Config;
+
+
+import net.legendahlupa.com.permtrading.PermTrading;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import javax.security.auth.login.Configuration;
+import java.io.File;
+
+public class CreatePermissionFile  {
+
+
+}
